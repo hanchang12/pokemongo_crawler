c5e902458ac0b3026108aba20665e387586e253d
hanchang12 <118660375+hanchang12@users.noreply.github.com>
2025-04-30 01:06:43 +0900

    Create server.py

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+app = Flask(__name__)
+
+@app.route('/api/news')
+def crawl_news():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=chrome_options)
+
+    try:
+        driver.get("https://pokemongo.com/news?hl=ko")
+        time.sleep(2)
+        items = driver.find_elements(By.CSS_SELECTOR, "div.NewsList-content-block a")
+
+        result = ""
+        for item in items[:5]:
+            title = item.text
+            link = item.get_attribute("href")
+            result += f"<b>{title}</b><br><a href='{link}'>{link}</a><br><br>"
+
+        return result
+    finally:
+        driver.quit()
